61b2e625c5dd6d00f34306d2907958eca32fd2bb
Shivam Rana <4826shivam@gmail.com>
2025-10-25 18:57:36 +0530

    feat: exporting all from one place, i.e. schema, db and other utilities

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,6 +1,4 @@
-import { drizzle } from "drizzle-orm/neon-http";
-import { neon } from "@neondatabase/serverless";
-import * as schema from "./schema";
+export * from "./client";
+export * from "./schema";
 
-const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle(sql, { schema });
+export { eq, and, or, gt, sql } from "drizzle-orm";
