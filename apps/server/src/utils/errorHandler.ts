47aa63d0bbe50f151d18c9b3cb168ab75f92e996
Shivam Rana <4826shivam@gmail.com>
2025-10-25 18:56:06 +0530

    feat: error handling will act as a middleware

---
+++ apps/server/src/utils/errorHandler.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from "express";
+import { logger } from "./logger";
+
+export const errorHandler = (
+  err: any,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
+  logger.error(err);
+  const status = err?.status || 500;
+  const message = err?.message || "Internal Server Error";
+  res.status(status).json({ ok: false, error: message });
+};
